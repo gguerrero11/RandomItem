3354175e5a201d0125848a6677356f3b070bc4eb
Gabriel Guerrero <gabrielguerrero@gabriels-mbp.attlocal.net>
2015-01-14 21:12:02 -0800

    Initial Commit

---
+++ RandomItems/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by Gabriel Guerrero on 1/14/15.
+//  Copyright (c) 2015 Gabriel Guerrero. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
